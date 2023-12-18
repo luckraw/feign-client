7813e155203db8fa70fd0b5065168064aa31dde1
nulld <krawdev@gmail.com>
2023-12-18 12:07:41 -0300

    feat: route GET products.

--- src/main/java/com/luckraw/feignclient/FeignClientApplication.java
+++ src/main/java/com/luckraw/feignclient/FeignClientApplication.java
@@ -2,8 +2,10 @@ package com.luckraw.feignclient;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cloud.openfeign.EnableFeignClients;
 
 @SpringBootApplication
+@EnableFeignClients
 public class FeignClientApplication {
 
 	public static void main(String[] args) {
--- src/main/java/com/luckraw/feignclient/infrastructure/ApiClient.java
+++ src/main/java/com/luckraw/feignclient/infrastructure/ApiClient.java
@@ -1,7 +1,14 @@
 package com.luckraw.feignclient.infrastructure;
 
+import com.luckraw.feignclient.apiv1.dto.ProductsDTO;
 import org.springframework.cloud.openfeign.FeignClient;
+import org.springframework.web.bind.annotation.GetMapping;
+
+import java.util.List;
 
 @FeignClient(value = "api-client", url = "${api-client.url:#{null}}")
 public interface ApiClient {
+
+    @GetMapping("/products")
+    public List<ProductsDTO> buscarProdutos();
 }
